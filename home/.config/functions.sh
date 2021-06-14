f66b12c047d06a2a987848367c3cc111e4bde603
vulkan-ops <vulkanops@gmail.com>
2021-06-14 17:23:21 -0300

    Revert "update function{sshgen}!"
    This reverts commit 6a2a7c063559b92aec2ca863fc0b17c16aea88fb.

--- home/.config/functions.sh
+++ home/.config/functions.sh
@@ -57,6 +57,7 @@ function sshgen() {
   ssh-keygen -t ed25519 -C "vulkanops@gmail.com"
   eval "$(ssh-agent -s)" && ssh-add -l
   cat $HOME/.ssh/id_ed25519.pub | wl-copy
+  xdg-open https://github.com/settings/ssh/new
 }
 
 function wifi() {
