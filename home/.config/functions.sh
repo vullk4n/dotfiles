ed31ca99148c946c82a11f3ec5e87e3c9b592ce6
vulkan-ops <vulkanops@gmail.com>
2022-03-18 19:26:05 -0300

    add more functions and fix commiter.sh!
    Signed-off-by: vulkan-ops <vulkanops@gmail.com>

--- home/.config/functions.sh
+++ home/.config/functions.sh
@@ -77,54 +77,60 @@ ssh vitor@casa.luizdores.com.br
 }
 
 function dt () {
+#git clone  https://github.com/LineageOS/android_hardware_motorola -b lineage-19.0  hardware/motorola
+git clone https://github.com/PixelExperience/external_bson  external/bson
 git clone https://github.com/PixelExperience/system_qcom  system/qcom
 git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996_display hardware/qcom-caf/msm8996/display
 git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996_media hardware/qcom-caf/msm8996/media
 git clone https://github.com/PixelExperience/hardware_qcom-caf_msm8996_audio hardware/qcom-caf/msm8996/audio
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/proton-clang
+#git clone https://gitlab.com/crdroidandroid/android_vendor_gapps vendor/gapps
+git clone https://github.com/PixelExperience/device_qcom_sepolicy-legacy-um  device/qcom/sepolicy-legacy-um
 }
 
-function derp () {
-#git clone https://github.com/vulkan-ops/device_motorola_channel -b derp  device/motorola/channel
-git clone https://github.com/vulkan-ops/device_motorola_ocean  -b derp  device/motorola/ocean
-#git clone https://github.com/vulkan-ops/device_motorola_river  -b derp  device/motorola/river
-git clone https://github.com/vulkan-ops/device_motorola_sdm632-common   device/motorola/sdm632-common
+function cr () {
+git clone https://github.com/vulkan-ops/device_motorola_channel -b 12.0  device/motorola/channel
+git clone https://github.com/vulkan-ops/device_motorola_ocean  -b 12.0  device/motorola/ocean
+git clone https://github.com/vulkan-ops/device_motorola_river  -b 12.0  device/motorola/river
+git clone https://github.com/vulkan-ops/device_motorola_sdm632-common -b twelve  device/motorola/sdm632-common
 git clone https://github.com/vulkan-ops/vendor_motorola_ocean   vendor/motorola/ocean
-#git clone https://github.com/vulkan-ops/vendor_motorola_channel   vendor/motorola/channel
-#git clone https://github.com/vulkan-ops/vendor_motorola_river   vendor/motorola/river
+git clone https://github.com/vulkan-ops/vendor_motorola_channel   vendor/motorola/channel
+git clone https://github.com/vulkan-ops/vendor_motorola_river   vendor/motorola/river
 git clone https://github.com/vulkan-ops/vendor_motorola_sdm632-common  vendor/motorola/sdm632-common
-git clone https://github.com/vulkan-ops/kernel_motorola_sdm632   kernel/motorola/sdm632
+git clone https://github.com/vulkan-ops/kernel_motorola_sdm632 -b twelve kernel/motorola/sdm632
 }
 
 function job () {
 cd Downloads
 mkdir github
 cd github
-git clone https://github.com/vulkan-ops/arch archlinux
-git clone https://github.com/vulkan-ops/device_motorola_ocean ocean-dt
-git clone https://github.com/vulkan-ops/device_motorola_channel	channel-dt
-git clone https://github.com/vulkan-ops/device_motorola_river	river-dt
-git clone https://github.com/vulkan-ops/device_motorola_sdm632-common  common-tree
-git clone https://github.com/vulkan-ops/vendor_motorola_ocean  vendor-ocean
-git clone https://github.com/vulkan-ops/vendor_motorola_river	vendor-river
-git clone https://github.com/vulkan-ops/vendor_motorola_channel	vendor-channel
-git clone https://github.com/vulkan-ops/vendor_motorola_sdm632-common vendor-common
-git clone https://github.com/vulkan-ops/kernel_motorola_sdm632 kernel
+git clone https://github.com/vulkan-ops/device_motorola_channel  channel-dt
+git clone https://github.com/vulkan-ops/device_motorola_ocean    ocean-dt
+git clone https://github.com/vulkan-ops/device_motorola_river    river-dt
+git clone https://github.com/vulkan-ops/device_motorola_sdm632-common common-tree
+git clone https://github.com/vulkan-ops/vendor_motorola_ocean   vendor-ocean
+git clone https://github.com/vulkan-ops/vendor_motorola_channel   vendor-channel
+git clone https://github.com/vulkan-ops/vendor_motorola_river   vendor-river
+git clone https://github.com/vulkan-ops/vendor_motorola_sdm632-common  vendor-common
+git clone https://github.com/vulkan-ops/kernel_motorola_sdm632   kernel
+git clone https://github.com/vulkan-ops/arch arch-script
 }
 
 function b () {
 . build/envsetup.sh
 #make installclean
-#export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
-#export SKIP_ABI_CHECKS=true
 lunch lineage_${1}-userdebug
 make  bacon -j$(nproc --all) | tee ${1}.log
+#make otapackage  | tee ${1}.log
 }
 
 function v () {
 . build/envsetup.sh
-#make installclean
 #export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
 #export SKIP_ABI_CHECKS=true
+#make installclean
 lunch derp_${1}-eng
+#make otapackage  | tee ${1}.log
 mka derp -j$(nproc --all) | tee ${1}.log
 }
+
--- home/.config/scripts/commiter.sh
+++ home/.config/scripts/commiter.sh
@@ -3,7 +3,7 @@
 github=/home/vitor/Downloads/github
 
 repo0="dotfiles"
-repo1="myarch"
+repo1="arch-script"
 repo2="ocean-dt"
 repo3="common-tree"
 repo4="vendor-ocean"
@@ -11,10 +11,9 @@ repo5="vendor-common"
 repo6="kernel"
 repo7="river-dt"
 repo8="vendor-river"
-repo9="official_devices"
-repo10="channel-dt"
-repo11="vendor-channel"
-repos="$repo0\n$repo1\n$repo2\n$repo3\n$repo4\n$repo5\n$repo6\n$repo7\n$repo8\n$repo9\n$repo10\n$repo11"
+repo9="channel-dt"
+repo10="vendor-channel"
+repos="$repo0\n$repo1\n$repo2\n$repo3\n$repo4\n$repo5\n$repo6\n$repo7\n$repo8\n$repo9\n$repo10"
 
 chosen="$(echo -e "$repos" | wofi --lines 15 --dmenu -p "  Commiter")"
 case $chosen in
@@ -37,10 +36,8 @@ alacritty -t newcommit --working-directory $github/river-dt;;
 $repo8)
 alacritty -t newcommit --working-directory $github/vendor-river;;
 $repo9)
-alacritty -t newcommit --working-directory $github/official_devices;;
-$repo10)
 alacritty -t newcommit --working-directory $github/channel-dt;;
-$repo11)
+$repo10)
 alacritty -t newcommit --working-directory $github/vendor-channel;;
 
 esac
