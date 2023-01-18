403ae62e2b8d503e47ac1a8deb3e19bca47b7a55
vulkan-ops <vulkanops@gmail.com>
2023-01-18 19:50:15 -0300

    add new bt device
    Signed-off-by: vulkan-ops <vulkanops@gmail.com>

--- home/.config/scripts/bluetooth.sh
+++ home/.config/scripts/bluetooth.sh
@@ -15,9 +15,9 @@ bt() {
 
 scan="Scan devices"
 bt1="WS-887"
+bt2="Pok3b0la"
 
-
-devices="$scan\n$bt1"
+devices="$scan\n$bt1\n$bt2"
 
 chosen="$(echo -e "$devices" | wofi --lines 5 --sort-order=DEFAULT --dmenu -p "  Bluetooth")"
 case $chosen in
@@ -25,5 +25,8 @@ case $chosen in
       bluetoothctl scan on;;
     $bt1)
       bt "69:03:AB:C5:00:4D";; # WS-887
+    $bt2)
+      bt "41:42:D2:B3:29:71";; # Pok3b0la
+
 esac
 exit 0;
