2ad58a809710fdb710c5fd9e8b3fabede9ed4cdd
vulkan-ops <vulkanops@gmail.com>
2023-01-23 16:55:14 -0300

    update commiter!
    Signed-off-by: vulkan-ops <vulkanops@gmail.com>

--- home/.config/scripts/commiter.sh
+++ home/.config/scripts/commiter.sh
@@ -2,43 +2,31 @@
 
 github=/home/vitor/Downloads/github
 
-repo0="dotfiles"
-repo1="arch-script"
-repo2="ocean-dt"
-repo3="common-tree"
-repo4="vendor-ocean"
+repo0="ocean-dt"
+repo1="river-dt"
+repo2="common-tree"
+repo3="vendor-ocean"
+repo4="vendor-river"
 repo5="vendor-common"
 repo6="kernel"
-repo7="river-dt"
-repo8="vendor-river"
-repo9="channel-dt"
-repo10="vendor-channel"
-repos="$repo0\n$repo1\n$repo2\n$repo3\n$repo4\n$repo5\n$repo6\n$repo7\n$repo8\n$repo9\n$repo10"
+repos="$repo0\n$repo1\n$repo2\n$repo3\n$repo4\n$repo5\n$repo6"
 
 chosen="$(echo -e "$repos" | wofi --lines 15 --dmenu -p "  Commiter")"
 case $chosen in
 $repo0)
-alacritty -t newcommit --working-directory $HOME/.dotfiles;;
-$repo1)
-alacritty -t newcommit --working-directory $github/myarch;;
-$repo2)
 alacritty -t newcommit --working-directory $github/ocean-dt;;
-$repo3)
+$repo1)
+alacritty -t newcommit --working-directory $github/river-dt;;
+$repo2)
 alacritty -t newcommit --working-directory $github/common-tree;;
-$repo4)
+$repo3)
 alacritty -t newcommit --working-directory $github/vendor-ocean;;
+$repo4)
+alacritty -t newcommit --working-directory $github/vendor-river;;
 $repo5)
 alacritty -t newcommit --working-directory $github/vendor-common;;
 $repo6)
 alacritty -t newcommit --working-directory $github/kernel;;
-$repo7)
-alacritty -t newcommit --working-directory $github/river-dt;;
-$repo8)
-alacritty -t newcommit --working-directory $github/vendor-river;;
-$repo9)
-alacritty -t newcommit --working-directory $github/channel-dt;;
-$repo10)
-alacritty -t newcommit --working-directory $github/vendor-channel;;
 
 esac
 exit 0;
