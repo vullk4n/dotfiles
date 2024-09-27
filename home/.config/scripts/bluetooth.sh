96bb24e2596be0562edcf9c61275bd998fbf6f02
vullk4n <vulkanops@gmail.com>
2024-09-27 13:27:28 -0300

    add new bt device
    Signed-off-by: vullk4n <vulkanops@gmail.com>

--- home/.config/scripts/bluetooth.sh
+++ home/.config/scripts/bluetooth.sh
@@ -14,7 +14,7 @@ bt() {
 }
 
 scan="Scan devices"
-bt1="WS-887"
+bt1="F0n3"
 bt2="Pok3b0la"
 
 devices="$scan\n$bt1\n$bt2"
@@ -24,7 +24,7 @@ case $chosen in
     $scan)
       bluetoothctl scan on;;
     $bt1)
-      bt "69:03:AB:C5:00:4D";; # WS-887
+      bt "1C:52:16:3F:B7:5C";; # F0n3
     $bt2)
       bt "41:42:D2:B3:29:71";; # Pok3b0la
 
